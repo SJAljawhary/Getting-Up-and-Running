7f46f6cad70f2f20240c6e4e3f730c34fc45e601
SJAljawhary <sndjehad@gmail.com>
2021-04-25 19:12:54 +0300

    addingcss

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 alert('welcome to our caffee !')
 
-var coffeType = prompt('what is your favourite coffee drink?'){
+let drinkType = prompt('do you want "hot coffee" or "cold coffee"?')
+if( drinkType == 'hot coffee'){
+    prompt('what coffee type do you want to order ?')
     alert('A delecious one !')
+    document.write(drinkType);
+}else if( drinkType == 'cold coffee'){
+    alert('good choice !')
+    document.write(drinkType);
+
 }
+
+
+let sugarAmount = prompt('Do you want it: "with sugar" or "without sugar" ?')
+if(sugarAmount == 'with sugar'){
+    prompt('how many sugar spoons do you want ?')
+    alert('your order will be prepared now !Please wait.')
+    console.log(sugarAmount);
+}else if (sugarAmount == 'without sugar'){
+    alert('your order will be prepared now !Please wait.')
+    console.log(sugarAmount);
+}
+
+
+
+
